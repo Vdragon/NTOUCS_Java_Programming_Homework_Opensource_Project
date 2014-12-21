b18d8d3cd65d971c402cefc8c656e69635768a1d
林博仁(09957010) <Henry.Lin.Taiwan@gmail.com>
2014-12-21 13:31:49 +0800

    支援程式參考文件產生器
    Signed-off-by: 林博仁(09957010) <Henry.Lin.Taiwan@gmail.com>

--- Source code/ntou/cs/java2014/HenryLin/CalculatorTest.java
+++ Source code/ntou/cs/java2014/HenryLin/CalculatorTest.java
@@ -1,11 +1,16 @@
 package ntou.cs.java2014.HenryLin;
-/* CalculatorTest.java
-   1-1 作業測試用程式 */
+/** @file CalculatorTest.java
+    @brief 1-1 作業 Calculator 物件類別測試程式
+    @author 林博仁(09957010) <Henry.Lin.Taiwan@gmail.com>
+    @copyright 本來源程式碼以 General Public Licence 授權條款第 3 版或其任一更近期版本釋出 */
 
+/** @brief CalculatorTest 物件類別 */
 public class CalculatorTest{
+	/** Java 程式進入點(entry point)
+	 *  
+	 *  @param args 從命令列傳進來的命令參數 */
 	public static void main(String[] args){
-		/* 呼叫標準輸出(standard output)(System.out)物件的負責輸出字串的 println 方法(method) 以輸出「Java 程式設計世界哈囉！」字串 */
-		System.out.println("1-1 作業測試用程式");
+			System.out.println("1-1 作業 Calculator 類別測試程式");
 		
 	}
 }
